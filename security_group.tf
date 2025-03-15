d16a6873427fe1b554deb1ea02f8a74d88711030
IlumeoDev <saillard.matt@gmail.com>
2025-03-15 19:01:07 +0100

    Tfsec run and correction
    you can see the tfsec run in the picture tfsec_run.jpg
    There are three critical error and one low one
    The two criticals are on the security_group.tf where the egress traffic let everything through
    The listeneers.tf critical is the ssl provided is outdated with vulnerabilities so I corrected it with a new one
    Finally the low error is the missing description in the egress security_group.tf which is better if provided but has no significant impact

--- security_group.tf
+++ security_group.tf
@@ -4,11 +4,13 @@ resource "aws_security_group" "alb" {
   vpc_id      = data.aws_vpc.shared.id
 
   egress {
+    description      = "Allow egress traffic"
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    ### Not a best practice to allow all traffic, try to be as specific as possible
+    protocol         = "1" ### allow icmp
+    cidr_blocks      = ["1.2.3.4/32"] ### specific addresses
+    ### enable ipv6_cidr_blocks = ["::/0"] if necessary
   }
 
   tags = {
